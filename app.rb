7aa154d6ceab077274afb516c389b325149a9507
rochellesagh <rsrochelli@gmail.com>
2018-08-14 15:10:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
-get '/medicines' do 
-  "Hello, World!"
+get '/patients' do 
+  @patients = patient.all
+  erb: patients/index
   #code that gets all medicines and renders the HTML file 
 end 
 
